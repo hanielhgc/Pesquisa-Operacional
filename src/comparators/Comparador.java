d3c4fae2438d51b0d9a8ad46aeb2863d7a56a44f
hanielhgc <haniel_sasuke@hotmail.com>
2017-11-12 21:08:59 -0200

    Comparator de Cromossomos para Collections.sort

---
+++ src/comparators/Comparador.java
@@ -0,0 +1,16 @@
+package comparators;
+import java.util.Comparator;
+
+import beans.Cromossomo;
+
+class Comparador implements Comparator<Cromossomo> {
+ 
+	
+	//ordem crescente
+	public int compare(Cromossomo o1, Cromossomo o2) {
+        if (o1.getFitness() > o2.getFitness()) return +1;
+        else if (o1.getFitness() < o2.getFitness()) return -1;
+        else return 0;
+    }
+
+}
